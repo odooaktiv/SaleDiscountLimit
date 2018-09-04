209c4c75e92e507088d0da796daf9ed8ed4d83a7
Odoo Aktiv <solankiriddhir@yahoo.in>
2018-09-04 16:36:19 +0530

    [ADD] Initial commit for sale discount limit for branch 10.0

---
+++ sale_discount_limit/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Part of AktivSoftware See LICENSE file for full copyright
+# and licensing details.
+{
+    'name': "Sale Discount Limit",
+    'summary': """
+            Sale Discount Limit based on Sales User Access Right.
+             """,
+    'description': """
+        Sale Discount Limit based on Sales User Access Right.
+        Like Sales Manager can giver maximum 15% Discount,
+        Sales User can give maximum 10% Discount.
+    """,
+    'author': "Aktiv Software",
+    'website': "http://www.aktivsoftware.com",
+    'category': 'Sales',
+    'version': '10.0.1.0.0',
+    'depends': ['sale'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/sales_discount_limit_view.xml',
+    ],
+    'images': ['static/description/banner.jpg'],
+    'installable': True,
+    'post_init_hook': '_fill_sales_discount_limit',
+}
